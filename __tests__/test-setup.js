c15c62607ee0f576b964313e216c3f22fbb64335
Enya-Sover <David.kagell@yh.nackademin.se>
2025-05-23 12:47:42 +0200

    lägger till UniqueConstraintError i tester som fungerar bättre än /unique/i. korrigerar stavningsfel i userSchemat

--- __tests__/test-setup.js
+++ __tests__/test-setup.js
@@ -1,16 +1,19 @@
-// test-setup.js
 process.env.NODE_ENV = "test";
 const sequelize = require("../src/config/database");
+// const Accommodation = require("../src/models/Accommodation"); 
 const User = require("../src/models/User");
-const Accommodation = require("../src/models/Accommodation");
-
 
 beforeAll(async () => {
-  await sequelize.sync();
+  await sequelize.sync({ force: true });
+});
+
+afterEach(async () => {
+  await User.destroy({ where: {} });
+  // await Accommodation.destroy({ where: {} });
 });
 
 afterAll(async () => {
   await sequelize.close();
 });
 
-module.exports = { sequelize, User, Accommodation };
+module.exports = { sequelize, User };
