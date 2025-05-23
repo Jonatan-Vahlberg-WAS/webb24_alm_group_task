079cd2442364b9efa8e41714b801c63c45c46524
ErikBergloef <erik.berglof2@hotmail.se>
2025-05-23 11:25:03 +0200

    added validation tests

--- __tests__/models/user.test.js
+++ __tests__/models/user.test.js
@@ -19,3 +19,18 @@ describe("Accommodation Model", () => {
   
 });
 
+it("should not allow users with the same username", async () => {
+  await User.create({username: "user", email: "user1@test.com"});
+
+  await expect(
+    User.create({username: "user", email: "user2@test.com"})
+  ).rejects.toThrow(/unique/i);
+});
+
+it("should not allow duplicate email adresses", async () => {
+  await User.create({ username: "user123", email: "unique_email@test.com" });
+
+  await expect(
+    User.create({ username: "user1234", email: "unique_email@test.com"})
+  ).rejects.toThrow(/unique/i);
+});
